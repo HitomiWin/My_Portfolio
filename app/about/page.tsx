0ea46ea0721f4e90ca9b6a737d18925fffe93e09
HitomiWin <hitomi.winberg@gmail.com>
2024-04-12 16:26:11 +0200

    fix background color to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Hero from "@/app/about/hero/hero";
 import Skills from "./skills/skills";
 export default function Page() {
+  const pathname = usePathname();
+  const headerStyle = pathname.substring(1);
   return (
-    <main>
+    <main className={headerStyle}>
       <Hero />
       <Skills />
     </main>
--- app/ui/header-nav.tsx
+++ app/ui/header-nav.tsx
@@ -1,10 +1,14 @@
+"use client";
 import React from "react";
+import { usePathname } from "next/navigation";
 import NavLinks from "./nav-links";
 import styles from "@/styles/header-nav.module.css";
 
 export default function HeaderNav() {
+  const pathname = usePathname();
+  const headerStyle = pathname.substring(1);
   return (
-    <header className={`${styles.header}`}>
+    <header className={`${styles.header} ${headerStyle ?? ""}`}>
       <div className={`${styles.container} wContainer`}>
         <NavLinks />
       </div>
